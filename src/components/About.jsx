316492bc14fa24a55dc4412405b0c3a4dd8c4cfe
Tiago Carvalho <136613404+tiago-bozo@users.noreply.github.com>
2025-02-13 17:59:33 -0300

    texto

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,7 +5,7 @@ import Button from "../layouts/Button";
 const About = () => {
   return (
     <div className=" min-h-screen flex flex-col items-center justify-center lg:px-32 px-5 bg-backgroundColor">
-      <h1 className=" font-semibold text-center text-4xl lg:mt-14 mt-24 mb-8">About Us</h1>
+      <h1 className=" font-semibold text-center text-4xl lg:mt-14 mt-24 mb-8">Sobre Nós</h1>
 
       <div className=" flex flex-col lg:flex-row items-center gap-5">
         <div className=" w-full lg:w-2/4">
@@ -13,7 +13,7 @@ const About = () => {
         </div>
         <div className=" w-full lg:w-2/4 p-4 space-y-3">
           <h2 className=" font-semibold text-3xl">
-            What Makes Our Coffee Special?
+          O que torna nosso café especial?
           </h2>
           <p>
             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Commodi
